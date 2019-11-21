7b7a16201c4721a1f72a4978623af0eeb1548a03
sohammondal <soham.pf.mondal@gmail.com>
2019-11-21 17:58:58 +0530

    change duration type to number

--- models/exercise.js
+++ models/exercise.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     description: { type: String, required: true },
-    duration: { type: String, required: true },
+    duration: { type: Number, required: true },
     date: { type: Date, default: new Date() }
 });
 
