ef866c9d3e753c805d0e1321972911dd4c716c12
sohammondal <soham.pf.mondal@gmail.com>
2019-11-22 17:44:32 +0530

    change date format

--- models/exercise.js
+++ models/exercise.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
+    _id: { type: String, required: true },
     userId: { type: String, required: true },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
-    date: { type: Date, default: new Date().toLocaleDateString('en-GB', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }) }
+    date: { type: Date }
 });
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
