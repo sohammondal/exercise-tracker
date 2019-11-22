4aa5731d70199e64383515ca1c58e92aa5bd15cd
sohammondal <soham.pf.mondal@gmail.com>
2019-11-22 10:12:11 +0530

    changed date format

--- models/exercise.js
+++ models/exercise.js
@@ -4,7 +4,7 @@ const exerciseSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     description: { type: String, required: true },
     duration: { type: Number, required: true },
-    date: { type: Date, default: new Date() }
+    date: { type: Date, default: new Date().toLocaleDateString('en-GB', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }) }
 });
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     username: { type: String, required: true },
-    createdAt: { type: Date, default: new Date() }
+    createdAt: { type: Date, default: new Date().toLocaleDateString('en-GB', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }) }
 })
 
 module.exports = mongoose.model('ExerciseUser', userSchema);
