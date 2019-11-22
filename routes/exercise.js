33ba0e7085baec22e8fffc042e19cc3585df451b
sohammondal <soham.pf.mondal@gmail.com>
2019-11-22 17:45:50 +0530

    update get logs route

--- routes/exercise.js
+++ routes/exercise.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { check, validationResult, query } = require('express-validator');
-const { addExerciseToUser } = require('../controllers/exercise');
+const { addExerciseToUser, getExerciseLogsOfUser } = require('../controllers/exercise');
 
 router.post('/add', [
     check('userId')
@@ -41,13 +41,18 @@ router.get('/log', [
         .optional().isISO8601().toDate().withMessage('From must be in the format YYYY-MM-DD'),
     query('limit')
         .optional().isInt().withMessage('Limit must be a numberic integer')
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
     const { userId, from, to, limit } = req.query;
-    return res.status(200).json({ userId, from });
+    try {
+        const resp = await getExerciseLogsOfUser(userId, from, to, parseInt(limit));
+        return res.status(200).json(resp);
+    } catch (error) {
+        return res.status(error.status || 500).json(error.message || 'Internal Server Error');
+    }
 })
 
 module.exports = router;
