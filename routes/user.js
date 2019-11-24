57f26a8e07e9c93cb121e88d320190008fc06a59
sohammondal <soham.pf.mondal@gmail.com>
2019-11-24 09:25:33 +0530

    add comments

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const { addUser, deleteUser, getUsers } = require('../controllers/user');
 
+
+/**
+* @api {post} /api/exercise/new-user
+* @apiName Create new user
+* @apiPermission open
+* @apiGroup User
+*
+* @apiBody  {object}  { username: 'Some Name' }
+*
+* @apiSuccess (201) {Object} mixed `User` object
+* @apiFaliure (409) {String} Username already exists
+* @apiFaliure (500) {String} Internal Server Error
+*/
+
 router.post('/new-user', async (req, res) => {
     if (req.body.username) {
         try {
@@ -14,6 +28,19 @@ router.post('/new-user', async (req, res) => {
     }
 });
 
+/**
+* @api {del} /api/exercise/user/:userId
+* @apiName Delete a user
+* @apiPermission open
+* @apiGroup User
+*
+* @apiParam  {String} [userId] userId
+*
+* @apiSuccess (200)
+* @apiFaliure (404) {String} User not found
+* @apiFaliure (500) {String} Internal Server Error
+*/
+
 router.delete('/user/:id', async (req, res) => {
     if (req.params.id) {
         try {
@@ -26,6 +53,19 @@ router.delete('/user/:id', async (req, res) => {
         res.status(400).json('User id cannot be empty');
     }
 })
+
+
+/**
+* @api {get} /api/exercise/users
+* @apiName Get all users
+* @apiPermission open
+* @apiGroup User
+*
+* @apiParam  -
+*
+* @apiSuccess (200) {Array} Users List
+* @apiFaliure (500) {String} Internal Server Error
+*/
 
 router.get('/users', async (req, res) => {
     try {
